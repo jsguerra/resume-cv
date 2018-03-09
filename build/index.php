f04230f737dbe3bf3afb6d379b1e5739b80cba22
Jose Guerra <jose@gcostudios.com>
2018-03-09 14:53:38 +0000

    Added jquery

--- build/index.php
+++ build/index.php
@@ -1,16 +1,34 @@
+<?php
+
+function push_to_browser($as, $uri) {
+    header('Link: ' . $uri . '; rel=preload; as=' . $as, false);
+}
+
+$assets = array(
+    '<//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700,700i,900,900i>' => 'style',
+    '</css/normalize.css>' => 'style',
+    '/css/style.css' => 'style',
+    '<//code.jquery.com/jquery-3.3.1.min.js>' => 'script',
+    '</js/jquery-3.3.1.min.js>' => 'script'
+);
+
+array_walk( $assets, push_to_browser);
+
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <title>Jos&eacute; Guerra - London based Web Developer</title>
         <meta name="description" content="Hi I'm Jose, a web developer in London who specializes in front-end and WordPress development.">
         <meta name="author" content="Jose Guerra">
 
-		<link type="text/css" rel="stylesheet" href="css/normalize.css" />
-        <link type="text/css" rel="stylesheet" href="css/style.css" media="all" />
+		<link rel="stylesheet" href="css/normalize.css" />
+        <link rel="stylesheet" href="css/style.css" media="all" />
     </head>
 
     <body>
@@ -22,6 +40,11 @@
             <p>Copyright <?php echo date('Y'); ?></p>
         </footer>
 
-        <script type="text/javascript" src="js/common.js"></script>
+		<script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous"></script>
+		<script>window.jQuery || document.write('<script src="js/jquery-3.3.1.min.js"><\/script>');</script>
+        <script type="text/javascript" src="js/script.js"></script>
     </body>
 </html>
