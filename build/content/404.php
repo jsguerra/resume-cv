5544a20a2497f2c24fd5d088ee6f7e6a1e7fa305
Jose Guerra <jose@gcostudios.com>
2018-03-12 22:26:24 +0000

    initial commit

---
+++ build/content/404.php
@@ -0,0 +1,21 @@
+		<header class="siteHeader">
+			<div class="header__top">
+				<div class="container flex-grid">
+					<div class="header__title">
+						<h1><?php siteName(); ?></h1>
+						<p>Front End Developer</p>
+					</div>
+					<div class="header__cta">
+						<a class="button" href="#"><i class="icon-home"></i> Home</a>
+					</div>
+				</div>
+			</div>
+			<div class="header__bottom container">
+				<img class="profileAvatar alignleft" src="http://localhost/resume-cv/build/images/avatar.jpg" alt="Jose Guerra" />
+				<p>I am a self-driven individual who enjoys challenges and problem solving. I develope responsive websites, pixel perfect designs, SEO and ecommerce websites.</p>
+			</div>
+		</header>
+		<main class="main container">
+			<h2>Error 404</h2>
+			<p>Something went wrong. Error 404 - This page does not exist.</p>
+		</main>
